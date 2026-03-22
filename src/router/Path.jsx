507276661a62d6aed0cbc8e60fd5bc5f11d509cf
pokujopo <pokujopo@gmail.com>
2026-03-22 16:34:43 +0300

    Update Path.jsx

--- src/router/Path.jsx
+++ src/router/Path.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 //import Home from '../pages/Home.jsx'
-//import Post_project from '../pages/Post_project.jsx';
-//import Project_page from "../pages/Project_page";
+import Post_project from '../pages/Post_project.jsx';
+import Project_page from "../pages/Project_page";
 import { lazy, Suspense } from "react";
 const Home = lazy(() => import("../pages/Home"));
 const Layout = lazy(() => import("../layout/Layout"));
-const Project = lazy(() => import("../pages/Project_page"));
-const Post_project = lazy(() => import("../pages/Post_project"));
+//const Project_page = lazy(() => import("../pages/Project_page"))
+
 
 const repoName = import.meta.env.VITE_REPO_NAME || "";
     
@@ -19,21 +19,12 @@ const repoName = import.meta.env.VITE_REPO_NAME || "";
           path: "/",
           element: <Home></Home>,
         },
-        {
-          path: "/post_project",
-          element: <Post_project></Post_project>,
-        },
-        {
-          path: "/project",
-          element: <Project></Project>,
-        },
-        
         
       
       ]
        },
- //   { path: `/post_project`, element: (<Post_project />) },
- //   { path: `/project`, element: (<Project_page />)}
+    { path: `/post_project`, element: (<Post_project />) },
+    { path: `/project`, element: (<Project_page />)}
   ],  
-  { basename: "/" }
+  { basename: `/${repoName}` }
 )
